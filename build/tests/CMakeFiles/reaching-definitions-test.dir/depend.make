b77362be099cde74102ac76346d45c41974c9983
David Trabish <davidtr1037@gmail.com>
2017-05-07 12:31:19 +0300

    removing build

--- build/tests/CMakeFiles/reaching-definitions-test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for reaching-definitions-test.
-# This may be replaced when dependencies are built.
--- build/tools/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/tau/dg/tools
-# Build directory: /home/david/tau/dg/build/tools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
